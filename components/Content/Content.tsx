9cd501037422a848fea489dbcc430ea28780cdda
Alimuddin Hasan <alimudhasan@gmail.com>
2022-07-11 23:39:13 +0700

    feat: update Content layout

--- components/Content/Content.tsx
+++ components/Content/Content.tsx
@@ -6,14 +6,14 @@ function Content() {
     <div className='bg-very-pale-orange rounded-2xl p-5'>
       <h1 className='font-bold text-2xl'>Spending - Last 7 days</h1>
       <div>Chart</div>
-      <div className="mt-5 pt-5 border-t-2 border-cream">
+      <div className="mt-5 pt-5 border-t-2 border-cream flex justify-between">
         <div>
-          <p>Total this month</p>
-          <p>$478.33</p>
+          <p className="text-medium-brown mb-2">Total this month</p>
+          <p className="font-bold text-3xl">$478.33</p>
         </div>
-        <div>
-          <p>+2.4%</p>
-          <p>from last month</p>
+        <div className="flex flex-col items-end justify-end">
+          <p className="font-bold">+2.4%</p>
+          <p className="text-medium-brown">from last month</p>
         </div>
       </div>
     </div>
