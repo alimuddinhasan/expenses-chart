75561148d0cd92dab33b2e31764043288f0a618f
Alimuddin Hasan <alimudhasan@gmail.com>
2022-07-17 00:06:00 +0700

    feat: add Block to Chart

--- components/Chart/Chart.tsx
+++ components/Chart/Chart.tsx
@@ -1,12 +1,50 @@
 import React from "react";
 import PropTypes from "prop-types";
+import Block from "./components/Block/Block";
 
 interface IChartProps {
   data: { label: string; value: number }[];
 }
 
 function Chart() {
-  return <div>Chart</div>;
+  const data = [
+    {
+      day: "mon",
+      amount: 17.45,
+    },
+    {
+      day: "tue",
+      amount: 34.91,
+    },
+    {
+      day: "wed",
+      amount: 52.36,
+    },
+    {
+      day: "thu",
+      amount: 31.07,
+    },
+    {
+      day: "fri",
+      amount: 23.39,
+    },
+    {
+      day: "sat",
+      amount: 43.28,
+    },
+    {
+      day: "sun",
+      amount: 25.48,
+    },
+  ];
+  
+  return (
+    <div className='flex items-end justify-between h-40 gap-2'>
+      {data.map((item, index) => (
+        <Block key={index} label={item.day} value={item.amount} />
+      ))}
+    </div>
+  );
 }
 
 Chart.propTypes = {};
