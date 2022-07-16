eb11a175dae2dd9093c947237dec4b013184c54e
Alimuddin Hasan <alimudhasan@gmail.com>
2022-07-17 00:22:09 +0700

    feat: add hover function to Block

--- components/Chart/components/Block/Block.tsx
+++ components/Chart/components/Block/Block.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import PropTypes from "prop-types";
 
 interface IBlockProps {
@@ -8,14 +8,35 @@ interface IBlockProps {
 
 function Block({ label, value }: IBlockProps) {
   const blockRef = useRef<HTMLDivElement>(null);
+  const valueRef = useRef<HTMLDivElement>(null);
+  const [isMouseEnter, setMouseEnter] = useState(false);
+
   useEffect(() => {
     if (blockRef.current) {
       blockRef.current.style.height = `${Math.floor(value) * 2}px`;
     }
   }, []);
+
+  useEffect(() => {
+    if (valueRef.current) {
+      valueRef.current.style.display = isMouseEnter ? "block" : "none";
+    }
+  }, [isMouseEnter]);
+
   return (
-    <div className='flex-1 flex flex-col gap-2'>
-      <div className='bg-soft-red rounded-md hover:bg-opacity-50' ref={blockRef}></div>
+    <div className='flex-1 flex flex-col gap-2 items-center relative'>
+      <div
+        className='bg-dark-brown h-auto w-full p-1 rounded-md absolute -top-8'
+        ref={valueRef}
+      >
+        <p className='text-xs text-center text-very-pale-orange'>${value}</p>
+      </div>
+      <div
+        className='bg-soft-red rounded-md hover:bg-opacity-50 w-8'
+        onMouseEnter={() => setMouseEnter(true)}
+        onMouseLeave={() => setMouseEnter(false)}
+        ref={blockRef}
+      ></div>
       <p className='text-xs text-center'>{label}</p>
     </div>
   );
