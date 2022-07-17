50b208873191ae926d11163e6162712ceac02a0c
Alimuddin Hasan <alimudhasan@gmail.com>
2022-07-18 04:16:08 +0700

    test: update Chart unit test

--- components/Chart/Chart.tsx
+++ components/Chart/Chart.tsx
@@ -1,52 +1,18 @@
 import React from "react";
-import PropTypes from "prop-types";
 import Block from "./components/Block/Block";
 
 interface IChartProps {
   data: { label: string; value: number }[];
 }
 
-function Chart() {
-  const data = [
-    {
-      day: "mon",
-      amount: 17.45,
-    },
-    {
-      day: "tue",
-      amount: 34.91,
-    },
-    {
-      day: "wed",
-      amount: 52.36,
-    },
-    {
-      day: "thu",
-      amount: 31.07,
-    },
-    {
-      day: "fri",
-      amount: 23.39,
-    },
-    {
-      day: "sat",
-      amount: 43.28,
-    },
-    {
-      day: "sun",
-      amount: 25.48,
-    },
-  ];
-  
+function Chart({ data }: IChartProps) {
   return (
     <div className='flex items-end justify-between h-40 gap-2'>
       {data.map((item, index) => (
-        <Block key={index} label={item.day} value={item.amount} />
+        <Block key={index} label={item.label} value={item.value} />
       ))}
     </div>
   );
 }
-
-Chart.propTypes = {};
 
 export default Chart;
--- components/Chart/__test__/Chart.test.tsx
+++ components/Chart/__test__/Chart.test.tsx
@@ -1,9 +1,14 @@
 import { render, screen } from "@testing-library/react";
-import Chart from '../Chart';
+import Chart from "../Chart";
 
 describe("Chart", () => {
   it("should render Chart component", () => {
-    render(<Chart />)
-    expect(screen.getByText('Chart')).toBeInTheDocument();
+    const data = [
+      { label: "label1", value: 11 },
+      { label: "label2", value: 22 },
+    ];
+    render(<Chart data={data} />);
+    expect(screen.getByText("label1")).toBeInTheDocument();
+    expect(screen.getByText("label2")).toBeInTheDocument();
   });
 });
