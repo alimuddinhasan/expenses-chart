0c564ed86978b59adf8abe7ea015320152d0105d
Alimuddin Hasan <alimudhasan@gmail.com>
2022-07-18 04:17:02 +0700

    feat: update Content props

--- components/Content/Content.tsx
+++ components/Content/Content.tsx
@@ -1,26 +1,27 @@
 import React from "react";
-import PropTypes from "prop-types";
-import Chart from '../Chart/Chart';
+import Chart from "../Chart/Chart";
 
-function Content() {
+interface IContentProps {
+  data: { label: string; value: number }[];
+}
+
+function Content({ data }: IContentProps) {
   return (
     <div className='bg-very-pale-orange rounded-2xl p-5'>
       <h1 className='font-bold text-2xl'>Spending - Last 7 days</h1>
-      <Chart />
-      <div className="mt-5 pt-5 border-t-2 border-cream flex justify-between">
+      <Chart data={data} />
+      <div className='mt-5 pt-5 border-t-2 border-cream flex justify-between'>
         <div>
-          <p className="text-medium-brown mb-2">Total this month</p>
-          <p className="font-bold text-3xl">$478.33</p>
+          <p className='text-medium-brown mb-2'>Total this month</p>
+          <p className='font-bold text-3xl'>$478.33</p>
         </div>
-        <div className="flex flex-col items-end justify-end">
-          <p className="font-bold">+2.4%</p>
-          <p className="text-medium-brown">from last month</p>
+        <div className='flex flex-col items-end justify-end'>
+          <p className='font-bold'>+2.4%</p>
+          <p className='text-medium-brown'>from last month</p>
         </div>
       </div>
     </div>
   );
 }
-
-Content.propTypes = {};
 
 export default Content;
--- components/Content/__test__/Content.test.tsx
+++ components/Content/__test__/Content.test.tsx
@@ -3,11 +3,12 @@ import Content from "../Content";
 
 describe("Content", () => {
   it("should render Content component", () => {
-    render(<Content />)
-    expect(screen.getByText('Spending - Last 7 days')).toBeInTheDocument();
-    expect(screen.getByText('Total this month')).toBeInTheDocument();
-    expect(screen.getByText('$478.33')).toBeInTheDocument();
-    expect(screen.getByText('+2.4%')).toBeInTheDocument();
-    expect(screen.getByText('from last month')).toBeInTheDocument();
+    const data = [{ label: "test", value: 20 }];
+    render(<Content data={data} />);
+    expect(screen.getByText("Spending - Last 7 days")).toBeInTheDocument();
+    expect(screen.getByText("Total this month")).toBeInTheDocument();
+    expect(screen.getByText("$478.33")).toBeInTheDocument();
+    expect(screen.getByText("+2.4%")).toBeInTheDocument();
+    expect(screen.getByText("from last month")).toBeInTheDocument();
   });
 });
